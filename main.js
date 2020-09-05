400f3654a36314aa7cd5e08994b552a75b1a889d
土屋一心 <tsuchiya.isshin@win-techno.com>
2020-09-05 16:42:42 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+async function main() {
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
+
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The name of the audio file to transcribe
+  const fileName = './resources/audio-output-file.mp3';
+
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'MP3',
+    sampleRateHertz: 16000,
+    languageCode: 'ja-JP',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+}
+main().catch(console.error);
